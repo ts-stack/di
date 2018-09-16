f99a901a7dd6bc94bf0c86044f7006b84b43a699
KostyaTretyak <ktretiak.in.ua@gmail.com>
2018-09-17 01:56:19 +0300

    Added tests from https://github.com/mgechev/injection-js

---
+++ test/forward_ref_spec.ts
@@ -0,0 +1,18 @@
+/**
+ * @license
+ * Copyright Google Inc. All Rights Reserved.
+ *
+ * Use of this source code is governed by an MIT-style license that can be
+ * found in the LICENSE file at https://angular.io/license
+ */
+
+import { Type } from '../src/type';
+import { forwardRef, resolveForwardRef } from '../src/di';
+
+describe('forwardRef', function() {
+  it('should wrap and unwrap the reference', () => {
+    const ref = forwardRef(() => String);
+    expect(ref instanceof Type).toBe(true);
+    expect(resolveForwardRef(ref)).toBe(String);
+  });
+});
